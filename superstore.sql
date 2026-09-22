ffc1541f03d0afee889972f9053ed655f4a1c3f3
devi <132941807+devi624@users.noreply.github.com>
2026-09-22 12:06:36 +0200

    Refactor superstore SQL schema with INT types
    Updated data types for primary keys and foreign keys in the database schema.

--- superstore.sql
+++ superstore.sql
@@ -1,29 +1,33 @@
-CREATE Database superstore; 
+CREATE DATABASE superstore;
+
+USE superstore;
+
 CREATE TABLE customers (
-    customer_id VARCHAR(50) PRIMARY KEY,
+    customer_id INT PRIMARY KEY,
     name VARCHAR(50),
     segment VARCHAR(50),
     country VARCHAR(50),
     region VARCHAR(50)
 );
+
 CREATE TABLE products (
-    product_id VARCHAR(20) PRIMARY KEY,
+    product_id INT PRIMARY KEY,
     category VARCHAR(50),
     sub_category VARCHAR(50),
-    product_name VARCHAR(100),
+    product_name VARCHAR(100)
 );
-CREATE TABLE orders (
-    order_id VARCHAR(20) PRIMARY KEY,
-    customer_id VARCHAR(20),
-    product_id VARCHAR(20),
-    order_date VARCHAR(50),
-    ship_date VARCHAR(50)
-    sales VARCHAR(20),
-    quantity VARCHAR(20),
-    discount VARCHAR(50),
-    profit VARCHAR(50)
-    
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
-     FOREIGN KEY (product_id) REFERENCES products(product_id)
 
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    product_id INT,
+    order_date DATE,
+    ship_date DATE,
+    sales DECIMAL(10,2),      
+    quantity INT,
+    discount DECIMAL(4,2),   
+    profit DECIMAL(10,2),    
+    
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
